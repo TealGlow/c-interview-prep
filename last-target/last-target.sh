555e3216104c647c01c0826c982edffd3d215c2c
tealglow <alyssacomstock95@gmail.com>
2022-09-07 14:20:32 -0700

    add bash for last target testing

---
+++ last-target/last-target.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# compile
+gcc -std=c99 -o last-target last-target.c
+echo "1123444444456 4"
+./last-target 1123444444456 4
+
+echo "1123444444456 5"
+./last-target 1123444444456 5
+
+echo "1123444444456 7"
+./last-target 1123444444456 7
